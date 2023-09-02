5b98287ece3b7ab91d5f0d0f1ef607d6cca29c40
Krishna Shukla <121384288+kriishukla@users.noreply.github.com>
2023-09-02 17:24:19 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Delete without head pointer/delete-without-head-pointer.cpp
@@ -0,0 +1,133 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}*head;
+
+Node *findNode(Node* head, int search_for)
+{
+    Node* current = head;
+    while (current != NULL)
+    {
+        if (current->data == search_for)
+            break;
+        current = current->next;
+    }
+    return current;
+}
+
+
+void insert()
+{
+    int n,i,value;
+    Node *temp;
+    scanf("%d",&n);
+
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&value);
+        if(i==0)
+        {
+            head=new Node(value);
+            temp=head;
+            continue;
+        }
+        else
+        {
+            temp->next= new Node(value);
+            temp=temp->next;
+            temp->next=NULL;
+        }
+    }
+}
+
+/* Function to print linked list */
+void printList(Node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    cout << endl;
+}
+
+
+
+// } Driver Code Ends
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}*head;
+*/
+
+class Solution
+{
+public:
+    // Function to delete a node without any reference to head pointer.
+    void deleteNode(Node *del)
+    {
+        if (del == NULL)
+            return;
+
+        // If the node to be deleted is the last node, we cannot proceed.
+        if (del->next == NULL)
+            return;
+
+        // Copy the data from the next node to the node to be deleted.
+        del->data = del->next->data;
+
+        // Store the reference to the next node.
+        Node *temp = del->next;
+
+        // Point the next pointer of the current node to the next node's next.
+        del->next = del->next->next;
+
+        // Delete the next node.
+        delete temp;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+/* Drier program to test above function*/
+int main(void)
+{
+    /* Start with the empty list */
+
+    int t,k,n,value;
+    
+    scanf("%d",&t);
+    while(t--)
+    {
+        insert();
+        scanf("%d",&k);
+        Node *del = findNode(head, k);
+        Solution ob;
+        if (del != NULL && del->next != NULL)
+        {
+            ob.deleteNode(del);
+        }
+        printList(head);
+    }
+    return(0);
+}
+
+
+
+// } Driver Code Ends
