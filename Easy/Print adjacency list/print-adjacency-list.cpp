8e10e0d89f72142437c2b67222bc67e9611f5a05
Krishna Shukla <121384288+kriishukla@users.noreply.github.com>
2023-09-05 19:40:33 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Print adjacency list/print-adjacency-list.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution { 
+  public: 
+    // Function to return the adjacency list for each vertex. 
+    vector<vector<int>> printGraph(int V, vector<pair<int,int>>edges) { 
+        // Code here 
+        vector<vector<int>>ans; 
+        // vector<int>temp; 
+        unordered_map<int,vector<int>>mp; 
+        for(auto it:edges){ 
+            mp[it.first].push_back(it.second); 
+            mp[it.second].push_back(it.first); 
+        } 
+         
+        for(int i=0; i<V; i++){ 
+            ans.push_back(mp[i]); 
+        } 
+        return ans; 
+    } 
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<pair<int,int>>edges;
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            edges.push_back({u,v});
+        }
+        Solution obj;
+        vector<vector<int>> adj = obj.printGraph(V, edges);
+        for(int i=0;i<V;i++)
+        {
+            sort(adj[i].begin(),adj[i].end());
+            for(auto it:adj[i])
+                cout<<it<<" ";
+            cout<<endl;
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
