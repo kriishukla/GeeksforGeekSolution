f2f247b23b59d22a5f6cdd2ecec5e6df9856736c
Krishna Shukla <121384288+kriishukla@users.noreply.github.com>
2024-05-29 10:22:49 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find All Four Sum Numbers/find-all-four-sum-numbers.py
@@ -0,0 +1,61 @@
+#User function Template for python3
+
+# arr[] : int input array of integers
+# k : the quadruple sum required
+class Solution:
+    def fourSum(self, nums, target):
+        nums.sort()
+        results = []
+        n = len(nums)
+        
+        for i in range(n - 3):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, n - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                left, right = j + 1, n - 1
+                while left < right:
+                    total = nums[i] + nums[j] + nums[left] + nums[right]
+                    if total == target:
+                        results.append([nums[i], nums[j], nums[left], nums[right]])
+                        left += 1
+                        right -= 1
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -= 1
+                    elif total < target:
+                        left += 1
+                    else:
+                        right -= 1
+        
+        return results
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+#Main
+if __name__=='__main__':
+    t = int(input())
+    while t:
+        t-=1
+        n, k=map(int,input().split())
+        # print(n, k)
+        a=list(map(int,input().strip().split()))[:n]
+        # print(a)
+        ob=Solution()
+        ans=ob.fourSum(a, k)
+        # print(ans)
+        for v in ans:
+            for u in v:
+                print(u, end=" ")
+            print("$", end="")
+        if(len(ans)==0):
+            print(-1, end="")
+        print()
+        
+        
+
+# } Driver Code Ends
