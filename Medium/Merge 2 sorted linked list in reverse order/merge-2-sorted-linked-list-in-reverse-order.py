0a6bbff3e4e119534c75994d92b02757eb4e2258
Krishna Shukla <121384288+kriishukla@users.noreply.github.com>
2024-06-02 23:58:36 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Merge 2 sorted linked list in reverse order/merge-2-sorted-linked-list-in-reverse-order.py
@@ -0,0 +1,93 @@
+#User function Template for python3
+
+'''
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+'''
+
+class Solution:
+    def mergeResult(self, h1, h2):
+        dummy = curr = Node(0)  # dummy node
+
+        while h1 and h2:
+            if h1.data < h2.data:
+                curr.next = h1
+                h1 = h1.next
+            else:
+                curr.next = h2
+                h2 = h2.next
+            curr = curr.next
+
+        curr.next = h1 if h1 else h2
+
+        prev = None
+        curr = dummy.next
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+
+        return prev  
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class Llist:
+    def __init__(self):
+        self.head=None
+    
+    def insert(self,data,tail):
+        node=Node(data)
+        
+        if not self.head:
+            self.head=node
+            return node
+        
+        tail.next=node
+        return node
+        
+
+def printList(head):
+    while head:
+        print(head.data,end=' ')
+        head=head.next
+        
+if __name__ == '__main__':
+    t=int(input())
+    
+    for tcs in range(t):
+        
+        n1,n2=[int(x) for x in input().split()]
+        
+        arr1=[int(x) for x in input().split()]
+        ll1=Llist()
+        tail=None
+        for nodeData in arr1:
+            tail=ll1.insert(nodeData,tail)
+            
+            
+        arr2=[int(x) for x in input().split()]
+        ll2=Llist()
+        tail=None
+        for nodeData in arr2:
+            tail=ll2.insert(nodeData,tail)
+        
+        
+        ob = Solution()
+        resHead=ob.mergeResult(ll1.head,ll2.head)
+        printList(resHead)
+        print()
+        
+    
+    
+# } Driver Code Ends
