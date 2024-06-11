b0c7f79ddcd55332a994dfe29a2efc2066fbe806
Krishna Shukla <121384288+kriishukla@users.noreply.github.com>
2024-06-11 13:48:41 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Boundary Traversal of binary tree/boundary-traversal-of-binary-tree.py
@@ -0,0 +1,149 @@
+#User function Template for python3
+
+'''
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+'''
+class Solution:
+    def printBoundaryView(self, root):
+        if not root:
+            return []
+
+        def isLeaf(node):
+            return node and not node.left and not node.right
+        
+        def addLeftBoundary(node, res):
+            while node:
+                if not isLeaf(node):
+                    res.append(node.data)
+                if node.left:
+                    node = node.left
+                else:
+                    node = node.right
+        
+        def addLeaves(node, res):
+            if isLeaf(node):
+                res.append(node.data)
+            else:
+                if node.left:
+                    addLeaves(node.left, res)
+                if node.right:
+                    addLeaves(node.right, res)
+        
+        def addRightBoundary(node, res):
+            temp = []
+            while node:
+                if not isLeaf(node):
+                    temp.append(node.data)
+                if node.right:
+                    node = node.right
+                else:
+                    node = node.left
+            while temp:
+                res.append(temp.pop())
+        
+        boundary = []
+        
+        if not isLeaf(root):
+            boundary.append(root.data)
+        
+        addLeftBoundary(root.left, boundary)
+        addLeaves(root, boundary)
+        addRightBoundary(root.right, boundary)
+        
+        return boundary
+
+        
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# function should return a list containing the boundary view of the binary tree
+#{ 
+#  Driver Code Starts
+import sys
+
+import sys
+sys.setrecursionlimit(100000)
+#Contributed by Sudarshan Sharma
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+    
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        obj = Solution()
+        res = obj.printBoundaryView(root)
+        for i in res:
+            print (i, end = " ")
+        print('')
+# } Driver Code Ends
