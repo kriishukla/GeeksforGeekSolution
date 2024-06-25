81da1d162933f16f18b3bd814cb953a93c367b3c
Krishna Shukla <121384288+kriishukla@users.noreply.github.com>
2024-06-25 12:44:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.py
@@ -0,0 +1,36 @@
+#User function Template for python3
+
+
+class Solution:
+    
+    def knapSack(self, W, wt, val, n):
+        dp = [[0 for _ in range(W + 1)] for _ in range(n + 1)]
+        
+        for i in range(1, n + 1):
+            for w in range(1, W + 1):
+                if wt[i - 1] <= w:
+                    dp[i][w] = max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w])
+                else:
+                    dp[i][w] = dp[i - 1][w]
+        
+        return dp[n][W]        
+        
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+# Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        n = int(input())
+        W = int(input())
+        val = list(map(int,input().strip().split()))
+        wt = list(map(int,input().strip().split()))
+        ob=Solution()
+        print(ob.knapSack(W,wt,val,n))
+# } Driver Code Ends
